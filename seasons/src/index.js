180565420e24756561191e2701b00494aa6ea0ab
Fuzoka <unbeatablesites09@gmail.com>
2019-01-24 17:41:43 -0500

    refactor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { REFUSED } from "dns";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    error => console.log(error)
-  );
-
-  return <div>Latitude: </div>;
-};
+class App extends React.Componet {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
+    return <div>Latitude</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
