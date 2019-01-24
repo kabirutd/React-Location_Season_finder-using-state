ec483b05ba9c92bb35e3b99c91e8857a89f76103
Fuzoka <unbeatablesites09@gmail.com>
2019-01-24 17:14:34 -0500

    configuration

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  window.navigator.geolocation.getCurrentPosition(position =>
-    console.log(position)
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
   );
 
   return <div>Hi there</div>;
