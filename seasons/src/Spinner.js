fae72c258846fb2983c264e6215421f72222856f
Fuzoka <unbeatablesites09@gmail.com>
2019-01-31 14:37:36 -0500

    Adding spinner

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+const spinner = () => {
+  return (
+    <div>
+      div class="ui segment">
+      <div class="ui active dimmer">
+        <div class="ui massive text loader">Loading...Yes!</div>
+      </div>
+    </div>
+  );
+};
+
+export default spinner;
