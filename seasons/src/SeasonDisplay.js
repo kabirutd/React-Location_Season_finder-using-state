1bf0f0d80ea8954a0841279625aa738780f33eac
Fuzoka <unbeatablesites09@gmail.com>
2019-01-31 13:49:00 -0500

    Change size of icon

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun"
+  },
+
+  winter: {
+    text: "Burr it is cold",
+    iconName: "snowflake"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,11 +22,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, its chilly" : "Lets hit the beach";
-
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
+      <i className={` massive ${iconName} icon`} />
       <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`} />
     </div>
   );
 };
